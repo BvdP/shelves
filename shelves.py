0c1000133e787ea681a34c699a6b194ae44385dc
Bastiaan van der Peet <bas_vanderpeet@yahoo.co.uk>
2015-02-11 15:03:24 +0100

    tabbed box works (no  margins yet)

--- shelves.py
+++ shelves.py
@@ -108,10 +108,10 @@ class Coordinate:
 
     def __str__(self):
         return "(%f, %f)" % (self.x, self.y)
-    
+
     def __add__(self, other):
         return Coordinate(self.x + other.x, self.y + other.y)
-        
+
     # def __radd__(self, other):
     #    return self + other
 
@@ -120,7 +120,7 @@ class Coordinate:
 
     def __mul__(self, factor):
         return Coordinate(self.x * factor, self.y * factor)
-        
+
     # def __rmul__(self, other):
     #     return self * other
 
@@ -143,7 +143,7 @@ class Shelves(inkex.Effect):
         self.OptionParser.add_option('--tolerance', action = 'store',
           type = 'float', dest = 'tolerance', default = '0.05',
           help = '')
-          
+
         self.OptionParser.add_option('--thickness', action = 'store',
           type = 'float', dest = 'thickness', default = '1.2',
           help = 'Material thickness')
@@ -163,15 +163,15 @@ class Shelves(inkex.Effect):
         self.OptionParser.add_option('--shelves', action = 'store',
           type = 'string', dest = 'shelve_list', default = '',
           help = 'semicolon separated list of shelve heigths')
-          
+
         self.OptionParser.add_option('--groove_depth', action = 'store',
           type = 'float', dest = 'groove_depth', default = '0.5',
           help = 'Groove depth')
-          
+
         self.OptionParser.add_option('--tab_size', action = 'store',
           type = 'float', dest = 'tab_size', default = '10',
           help = 'Approximate tab width (tabs will be evenly spaced along the length of the edge)')
- 
+
     try:
         inkex.Effect.unittouu   # unitouu has moved since Inkscape 0.91
     except AttributeError:
@@ -188,21 +188,21 @@ class Shelves(inkex.Effect):
 
         # input sanity check and unit conversion
         error = False
-        
+
         if self.options.unit not in self.knownUnits:
             inkex.errormsg('Error: unknown unit. '+ self.options.unit)
             error = True
         unit = self.options.unit
-        
+
         if min(self.options.height, self.options.width, self.options.depth) == 0:
             inkex.errormsg('Error: Dimensions must be non zero')
             error = True
 
         shelves = []
-        
+
         for s in self.options.shelve_list.split(';'):
             try:
-                float(s)  
+                float(s)
                 shelves.append(self.unittouu(str(s) + unit))
             except ValueError:
                 inkex.errormsg('Error: nonnumeric value in shelves (' + s + ')')
@@ -210,7 +210,7 @@ class Shelves(inkex.Effect):
 
         if error:
             exit()
-        
+
         height = self.unittouu(str(self.options.height) + unit)
         width = self.unittouu(str(self.options.width) + unit)
         depth = self.unittouu(str(self.options.depth) + unit)
@@ -229,7 +229,7 @@ class Shelves(inkex.Effect):
 
         def H(x):
             return Coordinate(x, 0)
-            
+
         def V(x):
             return Coordinate(0, x)
 
@@ -239,29 +239,82 @@ class Shelves(inkex.Effect):
                 n = F // 2
             else:
                 n = (F - 1) // 2
-            
+
             return 2 * n + 1
-            
+
+        # create groups for the different parts
+        g_l_side = inkex.etree.SubElement(layer, 'g')
+        g_r_side = inkex.etree.SubElement(layer, 'g')
+        g_top = inkex.etree.SubElement(layer, 'g')
+        g_bottom = inkex.etree.SubElement(layer, 'g')
+        g_back = inkex.etree.SubElement(layer, 'g')
+        g_divider = inkex.etree.SubElement(layer, 'g')
+
+
+        h_spacing = H(10 + thickness)
+        v_spacing = V(10 + thickness)
+
         v_tab_count = tab_count(height, tab_size)
         v_tab = V(height / v_tab_count)
         h_tab_count = tab_count(width, tab_size)
         h_tab = H(width / h_tab_count)
+        d_tab_count = tab_count(depth, tab_size)
+        d_tab_size = depth / d_tab_count
 
         h_tab_height = V(thickness)
-        
-        top_left = Coordinate(0, 0)
-        
-        start = top_left
-        for i in range(v_tab_count):
-            if i % 2 == 0:
-                t_offset = H(thickness)
-            else:
-                t_offset = H(0)
-            #start = top_left + t_offset + v_tab * i
-            end = start + v_tab
-            draw_SVG_line(layer, start, end)
-            start = top_left + t_offset + v_tab * (i + 1)
-            draw_SVG_line(layer, end, start)
+
+        top_origin = h_spacing * 2 + v_spacing + H(depth)
+        left_side_origin = h_spacing + v_spacing * 2 + V(depth)
+        back_origin = left_side_origin +  h_spacing + H(depth)
+        right_side_origin = back_origin + h_spacing + H(width)
+        bottom_origin = back_origin + v_spacing + V(height)
+
+        def draw_tabbed_edge(parent, edge_start, tab, inset, count, invert = False):
+            start = edge_start + (inset if invert else Coordinate(0, 0))
+            for i in range(count):
+                if (i % 2 == 0) != invert:
+                    t_offset = inset
+                else:
+                    t_offset = Coordinate(0, 0)
+                end = start + tab
+                #inkex.debug(str((i, start, end, t_offset)))
+                draw_SVG_line(parent, start, end)
+                if i < count - 1:   # skip last one
+                    start = edge_start + t_offset + tab * (i + 1)
+                    draw_SVG_line(parent, end, start)
+
+        # top
+        draw_SVG_line(g_top, top_origin, top_origin + H(width))
+        draw_tabbed_edge(g_top, top_origin, V(d_tab_size), H(thickness), d_tab_count, False)
+        draw_tabbed_edge(g_top, top_origin + H(width) , V(d_tab_size), H(-thickness), d_tab_count, False)
+        draw_tabbed_edge(g_top, top_origin + V(depth), h_tab, V(-thickness), h_tab_count, True)
+
+
+        # left
+        draw_SVG_line(g_l_side, left_side_origin, left_side_origin + V(height))
+        draw_tabbed_edge(g_l_side, left_side_origin + H(depth), v_tab, H(-thickness), v_tab_count, True)
+        draw_tabbed_edge(g_l_side, left_side_origin, H(d_tab_size), V(thickness), d_tab_count, True)
+        draw_tabbed_edge(g_l_side, left_side_origin + V(height), H(d_tab_size), V(-thickness), d_tab_count, True)
+
+        # back
+        draw_tabbed_edge(g_back, back_origin, v_tab, H(thickness), v_tab_count, False)
+        draw_tabbed_edge(g_back, back_origin + H(width), v_tab, H(-thickness), v_tab_count, False)
+        draw_tabbed_edge(g_back, back_origin, h_tab, V(thickness), h_tab_count, False)
+        draw_tabbed_edge(g_back, back_origin + V(height), h_tab, V(-thickness), h_tab_count, False)
+
+        # right
+        draw_SVG_line(g_r_side, right_side_origin + H(depth), right_side_origin + H(depth) + V(height))
+        draw_tabbed_edge(g_r_side, right_side_origin, v_tab, H(thickness), v_tab_count, True)
+        draw_tabbed_edge(g_r_side, right_side_origin, H(d_tab_size), V(thickness), d_tab_count, True)
+        draw_tabbed_edge(g_r_side, right_side_origin + V(height), H(d_tab_size), V(-thickness), d_tab_count, True)
+
+        # bottom
+        draw_SVG_line(g_bottom, bottom_origin + V(depth), bottom_origin + V(depth) + H(width))
+        draw_tabbed_edge(g_bottom, bottom_origin, V(d_tab_size), H(thickness), d_tab_count, False)
+        draw_tabbed_edge(g_bottom, bottom_origin + H(width) , V(d_tab_size), H(-thickness), d_tab_count, False)
+        draw_tabbed_edge(g_bottom, bottom_origin, h_tab, V(thickness), h_tab_count, True)
+
+
 
 # Create effect instance and apply it.
 effect = Shelves()
